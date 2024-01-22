82fca6e7e188dd77623523b98bbfc2daf7b0ddce
yacody <146646846+yacody@users.noreply.github.com>
2024-01-22 22:12:23 +0300

    try#4

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
-document.addEventListener('DOMContentLoaded', function () {
+function myFunction() {
 	fetch('realindex.html')
 		.then((response) => response.text())
 		.then((html) => {
 			document.querySelector('body').innerHTML = html;
 		})
 		.catch((error) => console.error('Error fetching content:', error));
-});
+}
+
+const userAgent = navigator.userAgent;
+if (userAgent) {
+	fetch('index2.html')
+		.then((response) => response.text())
+		.then((html) => {
+			document.querySelector('body').innerHTML = html;
+		})
+		.catch((error) => console.error('Error fetching content:', error));
+}
